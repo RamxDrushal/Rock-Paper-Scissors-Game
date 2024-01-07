59c7279ae5cb84f643b9bdece123662311cc5945
Ramosh Samarawickrama <141574217+RamxDrushal@users.noreply.github.com>
2024-01-07 11:45:05 +0530

    Add files via upload

---
+++ Rock Paper & Scissors Game/Script.js
@@ -0,0 +1,81 @@
+const gameContainer = document.querySelector(".container"),
+  userResult = document.querySelector(".user_result img"),
+  cpuResult = document.querySelector(".cpu_result img"),
+  result = document.querySelector(".result"),
+  optionImages = document.querySelectorAll(".option_image"),
+  userScoreDisplay = document.getElementById("userScore"),
+  cpuScoreDisplay = document.getElementById("cpuScore");
+
+let userScore = 0;
+let cpuScore = 0;
+
+optionImages.forEach((image, index) => {
+  image.addEventListener("click", (e) => {
+    image.classList.add("active");
+
+    userResult.src = cpuResult.src = "images/rock.png";
+    result.textContent = "Wait...";
+
+    optionImages.forEach((image2, index2) => {
+      index !== index2 && image2.classList.remove("active");
+    });
+
+    gameContainer.classList.add("start");
+
+    let time = setTimeout(() => {
+      gameContainer.classList.remove("start");
+      let imageSrc = e.target.querySelector("img").src;
+      userResult.src = imageSrc;
+
+      let randomNumber = Math.floor(Math.random() * 3);
+      let cpuImages = ["images/rock.png", "images/paper.png", "images/scissors.png"];
+      cpuResult.src = cpuImages[randomNumber];
+
+      let cpuValue = ["R", "P", "S"][randomNumber];
+      let userValue = ["R", "P", "S"][index];
+
+      let outcomes = {
+        RR: "Draw",
+        RP: "Cpu",
+        RS: "User",
+        PP: "Draw",
+        PR: "User",
+        PS: "Cpu",
+        SS: "Draw",
+        SR: "Cpu",
+        SP: "User",
+      };
+      let outComeValue = outcomes[userValue + cpuValue];
+
+      result.textContent = userValue === cpuValue ? "Match Draw" : `${outComeValue} Won!!`;
+
+      // Update the score
+      if (outComeValue === 'User') {
+        userScore++;
+      } else if (outComeValue === 'Cpu') {
+        cpuScore++;
+      }
+
+      // Display the updated score
+      userScoreDisplay.textContent = userScore;
+      cpuScoreDisplay.textContent = cpuScore;
+
+    }, 2500);
+  });
+});
+
+// Play Again function
+function playAgain() {
+  userScore = 0;
+  cpuScore = 0;
+  userScoreDisplay.textContent = userScore;
+  cpuScoreDisplay.textContent = cpuScore;
+  result.textContent = "Let's Play!!";
+  optionImages.forEach((image) => {
+    image.classList.remove("active");
+  });
+  userResult.src = cpuResult.src = "images/rock.png";
+}
+
+
+
